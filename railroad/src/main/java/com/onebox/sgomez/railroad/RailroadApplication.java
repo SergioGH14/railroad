41d208a4850a11f1a1ee3958669360f4066e7dcd
sergiogomezherrera <sergiogomezherrera@localhost>
2019-10-26 20:18:33 +0200

    Initial Commit

---
+++ railroad/src/main/java/com/onebox/sgomez/railroad/RailroadApplication.java
@@ -0,0 +1,13 @@
+package com.onebox.sgomez.railroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailroadApplication.class, args);
+	}
+
+}
---
+++ railroad/src/test/java/com/onebox/sgomez/railroad/RailroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebox.sgomez.railroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
