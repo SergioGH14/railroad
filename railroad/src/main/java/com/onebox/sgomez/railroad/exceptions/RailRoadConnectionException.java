1055c31076c013608352f4b8d1589b4b8ace62a1
sergiogomezherrera <sergiogomezherrera@mbp-de-sergio.mshome.net>
2019-10-29 08:56:23 +0100

    [CLEAN] Removed unused imports and serial version added

--- railroad/src/main/java/com/onebox/sgomez/railroad/exceptions/RailRoadConnectionException.java
+++ railroad/src/main/java/com/onebox/sgomez/railroad/exceptions/RailRoadConnectionException.java
@@ -2,6 +2,11 @@ package com.onebox.sgomez.railroad.exceptions;
 
 public class RailRoadConnectionException extends Throwable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -511285748095897081L;
+
 	public RailRoadConnectionException(String errorMessage) {
 		super(errorMessage);
 	}
